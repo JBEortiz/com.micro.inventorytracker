b62f36c164b839a407e02d7905597aef88929607
JBEortiz <juan@berenguer.com>
2023-08-07 12:01:50 +0200

    add controllerAdvice

--- src/main/java/com/micro/inventorytracker/constants/ConstantsExceptions.java
+++
@@ -1,7 +0,0 @@
-package com.micro.inventorytracker.constants;
-
-public class ConstantsExceptions {
-    public static final String ITEM_NOT_EXIST= "El objeto selecionado no existe";
-    public static final String ITEMS_NOT_EXIST= "No existe ningun objeto";
-    public static final String INVENTORY_NOT_EXIST= "El inventario selecionado no existe";
-}
